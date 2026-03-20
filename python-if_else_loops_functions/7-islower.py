56953289b02fb30df2cc9f0227673eafbb87637d
vitasec <davidabdurahmanov38@gmail.com>
2026-03-21 02:28:52 +0400

    8ci task

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 
 def islower(c):
     """kicik herf oldugunnu yoxlama"""
-    if ord(c) > 97 and ord(c) < 123:
+    if ord(c) >= 97 and ord(c) <= 123:
         return True
     else:
         return False
