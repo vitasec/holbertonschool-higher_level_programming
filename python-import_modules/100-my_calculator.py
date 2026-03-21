a323a44727f71d9ed97dcd82b78393525a62018f
vitasec <davidabdurahmanov38@gmail.com>
2026-03-22 01:41:25 +0400

    my cli

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-import sys
-from my_calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
-    # Arqument sayini yoxla (fayl adi + 3 arqument = 4)
-    if len(sys.argv) != 4:
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    # Arqumentleri deyisenlere menimset
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    # Operatora gore funksiyani sec
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
-    else:
-        # Yanlis operator xetasi
+        exit(1)
+    op = argv[2]
+    if op is not "+" and op is not "-" and \
+       op is not "*" and op is not "/":
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    # Neticeni cap et
-    print("{} {} {} = {}".format(a, operator, b, result))
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if op is "+":
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    elif op is "-":
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+    elif op is "*":
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    elif op is "/":
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
