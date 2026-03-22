e60a35913cd2dc98148416b08254b89e6371cdc9
vitasec <davidabdurahmanov38@gmail.com>
2026-03-22 11:09:18 +0400

    11 task

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """Siyahidaki lazimi indexdeki ededi sil"""
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    retrun my_list
