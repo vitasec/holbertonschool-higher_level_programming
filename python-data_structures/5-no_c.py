7dac8defac2e6c507d7d57930e44de8b42a99af0
vitasec <davidabdurahmanov38@gmail.com>
2026-03-22 10:44:39 +0400

    5th task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     """Metnden butun C,c silen func"""
     new_string = ""
     for char in my_string:
-        if char != 'c' or char != 'C':
+        if char != 'c' and char != 'C':
             new_string += char
     return new_string
