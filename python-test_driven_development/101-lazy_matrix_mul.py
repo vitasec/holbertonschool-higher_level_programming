12bf0d811f4dba06b1a3ffa38f56e565a53cc0db
vitasec <davidabdurahmanov38@gmail.com>
2026-03-29 08:03:21 -0400

    advanecd 2

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Bu modul `lazy_matrix_mul` funksiyasını təqdim edir.
+NumPy modulundan istifadə edərək iki matrisin hasilini tapır.
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    NumPy istifadə edərək iki matrisi vurur.
+
+    Arqumentlər:
+        m_a (list of lists): Birinci matris
+        m_b (list of lists): İkinci matris
+
+    Qaytarır:
+        NumPy ndarray: Matrislərin hasili
+    """
+    return np.matmul(m_a, m_b)
