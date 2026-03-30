c062dcd159df66d514fab7c86165efb9e25ca0be
vitasec <davidabdurahmanov38@gmail.com>
2026-03-30 08:15:01 -0400

    New things is olcusu

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Bu modul ölçüsü olan bir Square (Kvadrat) sinfini təyin edir."""
+
+
+class Square:
+    """KSquare sinifii"""
+
+    def __init__(self, size):
+        """Square instance.
+
+        Args:
+            size: kavdratin olcusu
+        """
+        self.__size = size
