797b2d08fb77d2416c4d4a21277adac53d1666c8
vitasec <davidabdurahmanov38@gmail.com>
2026-03-30 08:22:17 -0400

    3th task

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Bu modul ölçüsü olan, yoxlanılan və sahəsi hesablanan bir Square (Kvadrat) sinfini təyin edir."""
+
+
+class Square:
+    """Kvadratı təmsil edən sinif."""
+
+    def __init__(self, size=0):
+        """Kvadratın yeni instance yarat
+
+        Args:
+            size (int): Kvadratın terefin uzunlugu default zerodu
+
+        Raises:
+            TypeError: Əgər size tam ədəd (integer) deyilsə.
+            ValueError: Əgər size 0-dan kiçikdirsə.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
+
+    def area(self):
+        """Kvadratın saheni tapir
+
+        Returns:
+            int: Kvadratın sahesi
+        """
+        return self.__size ** 2
