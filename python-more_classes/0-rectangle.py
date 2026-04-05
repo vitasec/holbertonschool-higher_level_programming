37f59a8e2f50ab8562a0f718f12f8b1c2b06561a
vitasec <davidabdurahmanov38@gmail.com>
2026-04-05 06:49:21 -0400

    created rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Bu modul boş bir Rectangle sinfini təyin edir.
+"""
+
+
+class Rectangle:
+    """
+    Düzbucaqlı (Rectangle) obyektləri yaratmaq üçün boş sinif.
+    """
+    pass
