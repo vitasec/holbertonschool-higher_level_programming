8f8c8fe7e4b356bf0b67b4ddafdd9a58d447f883
vitasec <davidabdurahmanov38@gmail.com>
2026-04-08 01:54:31 -0400

    Deidofff

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module defines BaseGeometry class with area method."""
+
+
+class BaseGeometry:
+    """A class representing base geometry."""
+
+    def area(self):
+        """Raises an Exception indicating area is not implemented."""
+        raise Exception("area() is not implemented")
