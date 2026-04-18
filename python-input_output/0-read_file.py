be1f192be931192fa461494c6e5a79c68e43181e
vitasec <davidabdurahmanov38@gmail.com>
2026-04-18 05:56:32 -0400

    oh

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,8 +3,10 @@
 read file
 """
 
+
 def read_file(filename=""):
-    """reads a file
+    """
+    reads a file
     """
     with open(filename, encoding='utf-8') as myFile:
         print(myFile.read(), end='')
