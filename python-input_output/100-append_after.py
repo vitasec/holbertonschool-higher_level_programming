09167fe48b4bad528967fb6fa9f41ab19bd56ef4
vitasec <davidabdurahmanov38@gmail.com>
2026-04-18 13:21:21 -0400

    devid

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains the append_after function.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line 
+    containing a specific string.
+    """
+    text = ""
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
