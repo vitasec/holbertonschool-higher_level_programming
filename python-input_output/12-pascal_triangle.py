de544f302f507c831e3d949c7b72391a4a17b9ea
vitasec <davidabdurahmanov38@gmail.com>
2026-04-18 13:19:09 -0400

    last

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Contains the pascal_triangle function.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle of n.
+    Returns an empty list if n <= 0.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+        
+        # Calculate the internal values of the row
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
