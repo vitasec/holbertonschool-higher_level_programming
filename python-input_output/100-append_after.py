cb4c4f0900fa38ca981d14f16bff9c92074d29cf
vitasec <davidabdurahmanov38@gmail.com>
2026-04-18 13:22:08 -0400

    100-append_after.py

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -6,7 +6,7 @@ Contains the append_after function.
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts a line of text to a file, after each line 
+    Inserts a line of text to a file,
     containing a specific string.
     """
     text = ""
