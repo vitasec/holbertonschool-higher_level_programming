ee82c1eeefc1332b5013de1b6de8b1c76cbb4c16
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 09:45:54 -0400

    13

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows.
+-- The script uses the tv_genres table to filter by the name 'Comedy'.
+SELECT tv_shows.title
+  FROM tv_shows
+       INNER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+
+       INNER JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+ WHERE tv_genres.name = 'Comedy'
+ ORDER BY tv_shows.title ASC;
