9eb2485fce9af905677f8f77ce2730249ef4915d
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 09:54:29 -0400

    Add 101-relationship_states_cities_list.py

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-# script that lists all State objects from the database hbtn_0e_6_usa
-# # Sintax: ./100-relationship_states_cities.py username password database_name
-# state_name_to_search
-# Used module sqlalchemy
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""Module to list all States and corresponding Cities"""
 from relationship_city import City
 from relationship_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
+# Run only when executed
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Creating the engine
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Creating the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Printing
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
