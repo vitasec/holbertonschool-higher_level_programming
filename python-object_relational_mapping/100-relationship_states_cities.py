56ea0336c0796c68015e8552bec2e2e52a929267
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 09:54:29 -0400

    Add 100-relationship_states_cities.py

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
-# script that lists all State objects from the database hbtn_0e_6_usa
-# # Sintax: ./100-relationship_states_cities.py username password database_name
-# state_name_to_search
-# Used module sqlalchemy
+"""Module for creating State California and City San Francisco"""
 
-import sys
+from relationship_city import City, Base
+from relationship_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_city import Base, City
-from relationship_state import State
+from sys import argv
 
+# Run only when executed
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Creating the engine
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Binding the database
     Base.metadata.create_all(engine)
+
+    # Creating the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.add(City(name="San Francisco", state=State(name="California")))
+    # Adding new city and state
+    new_state = State(name="California")
+    session.add(new_state)
     session.commit()
+
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    session.add(new_city)
+    session.commit()
+
+    # Commiting changes
+    session.commit()
+
+    # Closing the session
+    if session:
+        session.close()
