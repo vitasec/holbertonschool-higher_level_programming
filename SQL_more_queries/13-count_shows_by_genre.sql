d8838c06c3f5fa3200f696b59f9a1241d5e70aa8
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 05:42:04 -0400

    12

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
--- Select results of the search if not null and the count of each instancel
-SELECT tv_genres.name AS `genre`, COUNT(*) AS `number_shows`
-FROM tv_genres JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id IS NOT NULL
-GROUP BY tv_genres.name
-ORDER BY `number_shows` DESC;
-
+-- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Results must be sorted in descending order by the number of shows linked.
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
