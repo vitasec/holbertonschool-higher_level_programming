2c522a823cca4ffb6f2a095b4d94ee2c71c8e7de
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 09:54:29 -0400

    Add 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-# script that lists all State objects from the database hbtn_0e_6_usa
-# # Sintax: ./12-model_state_update_id_2.py username password database_name
-# state_name_to_search
-# Used module sqlalchemy
-
-import sys
+"""
+Module for New Mexico update.
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sys import argv
 
+from model_state import Base, State
+
+# Run only executed
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    # Engine creation with mysql and mysqldb DBAPI
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+
+    # Creating all classes in DB
+    Base.metadata.create_all(engine)
+
+    # Creating Session and its instance
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
+    # Changing state name to New Mexico
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
     session.commit()
+
+    # Closing the session
+    if session:
+        session.close()
