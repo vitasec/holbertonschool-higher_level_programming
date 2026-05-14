582cec4f6032a950eef1137b910396a65c664088
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 10:09:00 -0400

    Add task_02_logic.py

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        data = json.load(f)
+    return render_template('items.html', items=data["items"])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
