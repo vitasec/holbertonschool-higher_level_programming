268a73ca8bac148b46f65d8dbc162d5f9afda8c6
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 09:54:30 -0400

    Add 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-# script that lists all cities from the database hbtn_0e_4_usa
-# Sintax: ./4-cities_by_state.py username password database_name
-import sys
-import MySQLdb
+"""Module for Selecting cities"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT c.id, c.name, s.name FROM cities as c \
-               INNER JOIN states as s ON c.state_id = s.id \
-               ORDER BY c.id")
-    [print(city) for city in c.fetchall()]
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
+    )
+    cursor = db.cursor()
+
+    cursor.execute('SELECT c.id, c.name, s.name \
+                    FROM cities AS c \
+                    INNER JOIN states AS s ON s.id = c.state_id ')
+
+    for state in cursor.fetchall():
+        print(state)
+
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
