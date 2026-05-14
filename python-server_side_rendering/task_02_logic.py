f060dc7c3f5d3bac57bbf6512e3f91d6fc275dd1
vitasec <davidabdurahmanov38@gmail.com>
2026-05-14 10:11:39 -0400

    2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,28 +1,19 @@
-#!/usr/bin/python3
-
 import json
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 @app.route('/items')
 def items():
-    with open('items.json') as f:
-        data = json.load(f)
-    return render_template('items.html', items=data["items"])
+    try:
+        with open('items.json', 'r') as f:
+            data = json.load(f)
+        # "items" yoxdursa, boş siyahı ([]) götürürük
+        items_list = data.get("items", [])
+    except (FileNotFoundError, json.JSONDecodeError):
+        items_list = []
+
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
